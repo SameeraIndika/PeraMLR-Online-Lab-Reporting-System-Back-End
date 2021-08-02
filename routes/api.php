a518f7ce80ec8bc9cf8626e0d26f0408f3c273f1
SameeraIndika <sameeraindikaN@gmail.com>
2021-08-02 16:00:18 +0530

    Router list changed.

--- routes/api.php
+++ routes/api.php
@@ -2,12 +2,13 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
-use App\Http\Controllers\UserController;
 use App\Http\Controllers\AdminController;
-use App\Http\Controllers\TestsController;
-use App\Http\Controllers\MessagesController;
 use App\Http\Controllers\FileController;
+use App\Http\Controllers\LabreportsController;
+use App\Http\Controllers\MessagesController;
+use App\Http\Controllers\OfficersController;
+use App\Http\Controllers\TestsController;
+use App\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -35,14 +36,9 @@ Route::post('register', [UserController::class, 'register']);
 Route::post('login', [UserController::class, 'login']);
 Route::get('userInfoById/{email:email}', [UserController::class, 'getUserInfoById']);
 
-//Routs interact with AdminController
-Route::post('CreateAdminAccount', [AdminController::class, 'register']);
-Route::post('AdminLogin', [AdminController::class, 'login']);
-//Route::get('AdminProfile/{email:email}', [AdminController::class, 'getAdminInfoById']);
-
-
-
-
+//Routs interact with FileController
+Route::post('file', [FileController::class, 'file']);
+Route::get('view-images', [FileController::class, 'getFile']);
 
 //Routs interact with TestsController
 Route::get('tests-info', [TestsController::class, 'getTestInfo']);
@@ -50,19 +46,24 @@ Route::get('tests-info/{id}', [TestsController::class, 'getTestInfoById']);
 Route::post('add-test', [TestsController::class, 'addTest']);
 Route::post('update-test/{id}', [TestsController::class, 'updateTest']);
 Route::get('delete-test/{id}', [TestsController::class, 'deleteTest']);
-//Route::get('removeTest/{test_id:test_id}', [TestsController::class, 'removeTest']);
 
+//Routs interact with OfficersController
+Route::get('officers-info', [OfficersController::class, 'getOfficersInfo']);
+Route::get('officers-info/{id}', [OfficersController::class, 'getOfficersInfoById']);
+Route::post('add-officer', [OfficersController::class, 'addOfficer']);
+Route::post('update-officer/{id}', [OfficersController::class, 'updateOfficer']);
+Route::get('delete-officer/{id}', [OfficersController::class, 'deleteOfficer']);
 
-
-
+//Routs interact with LabreportsController
+Route::get('labreport', [LabreportsController::class, 'getLabreports']);
 
 //Routs interact with MessagesController
-Route::get('messageInfo', [MessagesController::class, 'getMessageInfo']);
+Route::get('messages-info', [MessagesController::class, 'getMessageInfo']);
 Route::post('send-message', [MessagesController::class, 'sendMessage']);
 
-//Routs interact with FileController
-//Route::get('messageInfo', [MessagesController::class, 'getMessageInfo']);
-Route::post('file', [FileController::class, 'file']);
-Route::get('ViewImages', [FileController::class, 'getFile']);
-
-
+/**
+ *  For Future Development
+ */
+//Routs interact with AdminController
+Route::post('create-adminAccount', [AdminController::class, 'register']);
+Route::post('admin-login', [AdminController::class, 'login']);
