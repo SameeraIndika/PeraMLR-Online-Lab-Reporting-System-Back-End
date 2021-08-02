aa9faf66b2be59864eee84a253065f9a747f8f7b
SameeraIndika <sameeraindikaN@gmail.com>
2021-08-02 16:41:03 +0530

    Migration table was created.

---
+++ database/migrations/2021_08_01_211929_create_officers_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOfficersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('officers', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('post');
+            $table->string('description');
+            $table->string('phone');
+            $table->string('email');
+            $table->string('profile');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('officers');
+    }
+}
